93955215ecbdcc54a57c042562398374e5219e62
Jaroslav Hranička <hranicka@outlook.com>
2016-12-01 15:19:48 +0100

    Install path fixes

--- extensions/ssh2-install.sh
+++ extensions/ssh2-install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Add config files
-echo "extension=ssh2.so" | sudo tee -a /usr/local/php7/etc/conf.d/modules.ini
+echo "extension=ssh2.so" | sudo tee -a /usr/local/php71/etc/conf.d/modules.ini
