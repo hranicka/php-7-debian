664992313691cb6e8669feed8c7adb63fc9e6dfb
Jaroslav Hranička <hranicka@outlook.com>
2016-12-01 15:34:06 +0100

    APCu: 5.1.7

--- extensions/apcu-install.sh
+++ extensions/apcu-install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd "$(dirname "$0")"
 
-/usr/local/php7/bin/pecl -C /usr/local/php7/etc/pear.conf install apcu-5.1.6
+/usr/local/php7/bin/pecl -C /usr/local/php7/etc/pear.conf install apcu-5.1.7
