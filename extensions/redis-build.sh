f31fedfd14ffbc39807218c2508dba35d081c663
Jaroslav Hranicka <me@hranicka.cz>
2017-08-19 17:11:12 +0200

    PHP 7.1.8

--- extensions/redis-build.sh
+++ extensions/redis-build.sh
@@ -1,18 +1,4 @@
 #!/bin/bash
 cd "$(dirname "$0")"
 
-# Dependencies
-sudo apt-get update
-sudo apt-get install -y \
-    redis-server
-
-git clone https://github.com/phpredis/phpredis.git
-cd phpredis
-git checkout php7
-git pull
-
-/usr/local/php7/bin/phpize
-./configure --with-php-config=/usr/local/php7/bin/php-config
-
-make
-sudo make install
+echo "Build is not needed. Run redis-install.sh"
--- extensions/ssh2-install.sh
+++ extensions/ssh2-install.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+cd "$(dirname "$0")"
+
+/usr/local/php7/bin/pecl -C /usr/local/php7/etc/pear.conf install ssh2-1.1.2
 
 # Add config files
 echo "extension=ssh2.so" | sudo tee -a /usr/local/php7/etc/conf.d/modules.ini
