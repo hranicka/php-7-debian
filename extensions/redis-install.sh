7cdc3851eb85ffe171e414e3402876ae72839c86
Jaroslav Hranička <me@hranicka.cz>
2018-02-19 12:26:16 +0100

    PHP 7.2.2

--- extensions/redis-install.sh
+++ extensions/redis-install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd "$(dirname "$0")"
 
-/usr/local/php7/bin/pecl -C /usr/local/php7/etc/pear.conf install redis-3.1.3
+/usr/local/php7/bin/pecl -C /usr/local/php7/etc/pear.conf install redis-3.1.6
 
 # Enable the module
 echo "extension=redis.so" | sudo tee -a /usr/local/php7/etc/conf.d/modules.ini
